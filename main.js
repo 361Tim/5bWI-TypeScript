7b0d081417fe95b55098faed5af7ad06421acb5d
Timothe Goldmann <timothe.goldmann@student.htldonrbirn.at>
2025-02-18 12:15:34 +0100

    Minute 30 im Video

--- main.js
+++ main.js
@@ -1,2 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var message = 'Hello World';
 console.log(message);
+var x = 10;
+var y = 20;
+var sum;
+var title = 'TypeScript';
+var isBeginner = true;
+var total = 0;
+var name = "TIm";
+var sentence = "My name is ".concat(name, "\nI am a beginner in Typescript");
+console.log(sentence);
+var n = null;
+var u = undefined;
+var list11 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ['Chris', 22];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 5] = "Red";
+    Color[Color["Green"] = 6] = "Green";
+    Color[Color["BLue"] = 7] = "BLue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+console.log(c);
+var randomValue = 10;
+randomValue = true;
+randomValue = 'Vishwas';
+var myVariable = 10;
+function hasName(obj) {
+    return !!obj &&
+        typeof obj === "object" &&
+        "name" in obj;
+}
+console.log(myVariable.name);
+myVariable();
+myVariable.toUpperCase();
--- main.ts
+++ main.ts
@@ -1,3 +1,46 @@
 export { };
 let message = 'Hello World';
 console.log(message);
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = 'TypeScript';
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = "TIm";
+
+let sentence: string = `My name is ${name}
+I am a beginner in Typescript`;
+
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let list11: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+
+let person1: [string, number] = ['Chris', 22];
+
+enum Color { Red = 5, Green, BLue };
+
+let c: Color = Color.Green;
+console.log(c);
+
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'Vishwas';
+
+let myVariable: unknown = 10;
+
+function hasName(obj: any): obj is { name: string } {
+    return !!obj &&
+        typeof obj === "object" &&
+        "name" in obj
+}
+console.log(myVariable.name);
+myVariable();
+(myVariable as string).toUpperCase();
